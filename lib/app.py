a8d44f8368235c7364c4a931e734876007d6ecdc
Ricardo Guerra <RikkuG491@gmail.com>
2024-03-12 12:11:34 -0400

    Added comments to solution code for Deliverables

--- lib/app.py
+++ lib/app.py
@@ -1,14 +1,18 @@
 import ipdb
 
+# Deliverable 1
 def combine_sequences(seq1, seq2):
     return seq1 + seq2
 
+# Deliverable 2
 def sequence_n_times(seq, n):
     return seq * n
 
+# Deliverable 3
 def average(seq):
     return sum(seq) / len(seq)
 
+# Deliverable 4
 def append_n_times(input_list, element, n):
     for i in range(n):
         input_list.append(element)
@@ -29,7 +33,10 @@ foods = [
     }
 ]
 
+# Deliverable 5
 food_names = [food['name'] for food in foods]
+
+# Deliverable 6
 food_prices = [food['price'] for food in foods]
 average_price = sum(food_prices) / len(food_prices)
 
@@ -48,4 +55,5 @@ animals = [
     }
 ]
 
+# Deliverable 7
 animal_descriptions = [f"{animal['name']} is a {animal['animal_type']}" for animal in animals]
