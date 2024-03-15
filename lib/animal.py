9f26b8bbe8da6cdf077aab74a5775b19c89faede
Ricardo Guerra <RikkuG491@gmail.com>
2024-03-15 12:23:29 -0400

    Added solution code for Deliverables

--- lib/animal.py
+++ lib/animal.py
@@ -2,21 +2,28 @@ import ipdb
 
 class Animal:
 
+    # Deliverable 3
     all = []
     
+    # Deliverable 1
     def __init__(self, name, age):
         self.name = name
         self.age = age
         
+        # Deliverable 3
         Animal.all.append(self)
 
+    # Deliverable 2
     def make_animal_sound(self):
         print("Animal Sound!")
 
+# Deliverable 4
 class Dog(Animal):
 
+    # Deliverable 7
     all = []
     
+    # Deliverable 5
     def __init__(self, name, age, bark_volume = 3, obedience_level = 3):
         super().__init__(name, age)
         self.obedience_level = obedience_level
@@ -24,30 +31,22 @@ class Dog(Animal):
 
         Dog.all.append(self)
 
+    # Deliverable 6
     def make_animal_sound(self):
-        # print("Bark" + f"{'!' * self.bark_volume}")
         print(f"Bark{'!' * self.bark_volume}")
 
+# Deliverable 8
 class Cat(Animal):
 
+    # Deliverable 10
     all = []
 
     def __init__(self, name, age):
         super().__init__(name, age)
 
+        # Deliverable 10
         Cat.all.append(self)
     
+    # Deliverable 9
     def make_animal_sound(self):
         print("Meow!")
-
-animal1 = Animal("Alice", 2)
-animal2 = Animal("Bob", 4)
-
-fido = Dog("Fido", 3)
-buddy = Dog("Buddy", 5, 4, 5)
-cooper = Dog("Cooper", 4, 5)
-
-kitty = Cat("Kitty", 4)
-sally = Cat("Sally", 7)
-
-ipdb.set_trace()
