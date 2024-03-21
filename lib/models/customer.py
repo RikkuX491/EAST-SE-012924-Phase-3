9b0a4738060c98aa620ecbe0de142ee8867ed71c
Ricardo Guerra <RikkuG491@gmail.com>
2024-03-21 13:17:36 -0400

    Added solution code

--- lib/models/customer.py
+++ lib/models/customer.py
@@ -30,11 +30,55 @@ class Customer:
             self._last_name = last_name_parameter
         else:
             raise ValueError("Last Name must be a string at least 1 character long!")
+        
+    def __repr__(self):
+        return f"Customer {self.id}: First Name = {self.first_name}, Last Name = {self.last_name}"
+    
+    # add new ORM methods after existing methods
 
+    @classmethod
+    def create_table(cls):
+        # Create a new table to persist the attributes of Customer instances
+        sql = """
+            CREATE TABLE IF NOT EXISTS customers (
+            id INTEGER PRIMARY KEY,
+            first_name TEXT,
+            last_name TEXT)
+        """
+        CURSOR.execute(sql)
+
+    @classmethod
+    def drop_table(cls):
+        # Drop the table that persists Customer instances
+        sql = """
+            DROP TABLE IF EXISTS customers;
+        """
+        CURSOR.execute(sql)
+
+    def save(self):
+        # Insert a new row with the name value of the current Customer instance.
+        # Update object id attribute using the primary key value of new row.
+        sql = """
+            INSERT INTO customers (first_name, last_name)
+            VALUES (?, ?)
+        """
+
+        CURSOR.execute(sql, (self.first_name, self.last_name))
+        CONN.commit()
+
+        self.id = CURSOR.lastrowid
+
+        Customer.all.append(self)
+
+    @classmethod
+    def create(cls, first_name, last_name):
+        # Initialize a new Customer instance and save the object to the database
+        customer = cls(first_name, last_name)
+        customer.save()
+        return customer
+    
     def reviews(self):
         pass
     
     def hotels(self):
         pass
-    
-    # add new ORM methods after existing methods
